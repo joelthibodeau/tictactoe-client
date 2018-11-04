b82032105e1bf8b24ad038feed3095464dcd3ea1
joelthibodeau <joel.thibodeau@gmail.com>
2018-11-04 14:01:47 -0500

    Tic Tac Toe Project • try simplified logic. Try more simple/readable logic checking for each individual win rather than shorthand winning combos.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,50 +7,88 @@
 // require('./example')
 
 // const authEvents = require('./auth/events.js')
-//
-// $(() => {
-//   $('#sign-up').on('submit', authEvents.onSignUp)
-//   $('#sign-in').on('submit', authEvents.onSignIn)
-//   $('#sign-out').on('submit', authEvents.onSignOut)
-// })
+// A $( document ).ready() block.
+
+
+$(document).ready(function () {
+
+  const player_x = 'X'
+  const player_o = 'O'
+  let movesMade = 0
+  let currentMove = 1
+
+  console.log('ready!')
+
+function checkForWinner () {
+    const cell0 = document.GetElementById('cell0')
+    const cell1 = document.GetElementById('cell1')
+    const cell2 = document.GetElementById('cell2')
+    const cell3 = document.GetElementById('cell3')
+    const cell4 = document.GetElementById('cell4')
+    const cell5 = document.GetElementById('cell5')
+    const cell6 = document.GetElementById('cell6')
+    const cell7 = document.GetElementById('cell7')
+    const cell8 = document.GetElementById('cell8')
+
+    // check for all possible wins
+    // check all rows
+    if (cell0.innerHTML !== '' && cell0.innerHTML === cell1.innerHTML && cell0.innerHTML === cell2.innerHTML) {
+      console.log('win')
+    } else if (cell3.innerHTML !== '' && cell3.innerHTML === cell4.innerHTML && cell3.innerHTML === cell5.innerHTML) {
+      console.log('win')
+    } else if (cell6.innerHTML !== '' && cell6.innerHTML === cell7.innerHTML && cell6.innerHTML === cell8.innerHTML) {
+      console.log('win')
+      // check all columns
+    } else if (cell0.innerHTML !== '' && cell0.innerHTML === cell3.innerHTML && cell0.innerHTML === cell6.innerHTML) {
+      console.log('win')
+    } else if (cell1.innerHTML !== '' && cell1.innerHTML === cell4.innerHTML && cell1.innerHTML === cell7.innerHTML) {
+      console.log('win')
+    } else if (cell2.innerHTML !== '' && cell2.innerHTML === cell5.innerHTML && cell2.innerHTML === cell8.innerHTML) {
+      console.log('win')
+      // check diagonal l–>r, r–>l
+    } else if (cell0.innerHTML !== '' && cell0.innerHTML === cell4.innerHTML && cell0.innerHTML === cell8.innerHTML) {
+      console.log('win')
+    } else if (cell2.innerHTML !== '' && cell2.innerHTML === cell4.innerHTML && cell2.innerHTML === cell6.innerHTML) {
+      console.log('win')
+    }
+  }
+})
+
+/*
 
 $(() => {
   // your JS code goes here
   const player_x = 'X'
   const player_o = 'O'
-
   let currentMove = 1
   let movesCompleted = 0
-
   const box = $('.box')
   const winnerContainer = $('.winner')
   const reset = $('.reset')
-  const makeMove = function  (e){
-    //code here
-}
-
+  const makeMove = function (e) {
+  // code here
+  }
   box.on('click', function () {
-
     movesCompleted++
-
     // finding odd numbers
-// if (event.target.innerHTML === '') { BEGIN FUNCTION FOR SECOND CLICK
-    if (currentMove % 2 === 1) {
-      event.target.innerHTML = player_x
-      // event.target.style.color = 'red'
-      currentMove++
-    } else {
-      event.target.innerHTML = player_o
-      // event.target.style.color = 'green'
-      currentMove--
+    if (event.target.innerHTML === '') { // BEGIN FUNCTION FOR SECOND CLICK
+      if (currentMove % 2 === 1) {
+        event.target.innerHTML = player_x
+        // event.target.style.color = 'red'
+        currentMove++
+        console.log('X')
+      } else {
+        event.target.innerHTML = player_o
+        // event.target.style.color = 'green'
+        currentMove--
+        console.log('O')
+      }
     }
     // $(this).off('click') TRIED USING .off
-
     // function boxClick(inner) {
     // if (inner.innerHTML === "X" || inner.innerHTML === "O") {
     //     return;
     // }
-
     if (checkForWinner()) {
       // if currentMove is equal to 1, player_o wins (because opposite), else player_x wins.
       const theWinner = currentMove === 1 ? player_o : player_x
@@ -58,9 +96,10 @@ $(() => {
     }
   // } END FUNCTION TO PREVENT SECOND CLICK
   })
-
   reset.on('click', function (e) {
-    const moves = Array.prototype.slice.call($('.inner'))
+    const moves = Array.prototype.slice.call($('.text'))
+    // gets array length?
+    // console.log(moves.length)
     moves.map((m) => {
       m.innerHTML = ''
     })
@@ -69,12 +108,12 @@ $(() => {
     currentMove = 1
     movesCompleted = 0
   })
-
   function checkForWinner () {
     if (movesCompleted > 4) {
-      const moves = Array.prototype.slice.call($('.inner'))
+      const moves = Array.prototype.slice.call($('.text'))
       const results = moves.map(function (box) {
         return box.innerHTML
+         // console.log(box.innerHTML)
       })
       const winCombos = [
         [0, 1, 2],
@@ -99,10 +138,13 @@ $(() => {
       })
     }
   }
-function declareWinner (winner) {
+  function declareWinner (winner) {
     winnerContainer.css('display', 'block')
     reset.css('display', 'block')
     winner = winner === player_x ? 'x' : 'o'
     winnerContainer.html(winner + ' wins!')
+    console.log(winner + ' wins!')
   }
 })
+
+*/
